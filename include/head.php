17ca8f83fd7288673972da474d1674aacef2cd9d
SkillUP Student <student@skillup.spb.ru>
2018-02-01 21:54:40 +0300

    lesson 4

---
+++ include/head.php
@@ -0,0 +1,6 @@
+
+<head>
+    <title><?= RPODJECT_NAME ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
--- index.php
+++ index.php
@@ -1,17 +1,89 @@
-<h1>Helloworld</h1>
 
 
 
 
 <?php
+
+define('SECOND_IN_DAY', 86400);
+
 $currentDate = time();
-$yesterday = $currentDate - 60*60*24;
+$yesterday = $currentDate - SECOND_IN_DAY * 7;
 
-echo $currentDate . "<br />";
-echo date('d.m.Y H:i:s') ."<br />";
-echo date('d.m.Y H:i:s', $yesterday);
 
-require_once ('template.php')
+//require_once ('template.php')
+
+define('PRODJECT_NAME', 'Skillup');
 ?>
 
-<p>some text  <?= $currentDate ?></p>
+
+<!DOCTYPE html>
+<html>
+<?php require_once 'include/head.php'; ?>
+<body>
+<div class="container">
+    <div class="header">
+        <div class="logo">
+            <img src="image/logo.png">
+        </div>
+        <div class="search">
+            <form>
+                <input  type="text" name="Search" value="<?= date('d.m.Y H:i:s') ?>">
+                <button class="inp">Поиск</button>
+            </form>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index.php">О нас</a></li>
+                <li><a href="index.php">Регистрация</a></li>
+                <li><a href="index.php">Войти</a></li>
+
+            </ul>
+        </div>
+
+    </div>
+    <div class="head_posts">
+        <div class="title">
+            <h1>Моя любимая еда</h1>
+        </div>
+        <div class="ava"><img src="image/ava.png"></div>
+        <div class="user">Петя заходил час назад</div>
+
+        <div class="post_img">
+            <img src="image/post.jpg">
+        </div>
+        <div class="footer_post">
+            <div class="like">
+                <a href="#"><img src="image/like.png"></a>
+            </div>
+            <div class="comments">
+                <a href="#">Оставить комментарий</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="head_posts">
+        <div class="title">
+            <h1>Моя любимая еда</h1>
+        </div>
+        <div class="ava"><img src="image/ava.png"></div>
+        <div class="user">Петя заходил час назад</div>
+
+        <div class="post_img">
+            <img src="image/post.jpg">
+        </div>
+        <div class="footer_post">
+            <div class="like">
+                <a href="#"><img src="image/like.png"></a>
+            </div>
+            <div class="comments">
+                <a href="#">Оставить комментарий</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+
+    </div>
+</div>
+</body>
+</html>
